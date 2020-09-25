cb91ab1402c94e1bd50e9316579e0fac2122c629
StGreg13 <13mrgreg@gmail.com>
2020-09-25 12:39:07 +0300

    part of tabs

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 $(document).ready(function(){
-    // var tab = $('.tab');
-    // tab.click(function(){
-    //     var target = $(this).attr('data-target');
-    //     $('.tab-content').hide();
-    //     $('.'+target).show();
-    // });
-
     var tab = $('.tab');
     tab.click(function(){
-        var target = $(this).attr('href');
-        console.log("эта ссылка ведет на " + target);
-        $('.content').hide();
-        $(target).show();
+        var target = $(this).attr('data-target');
+        $('.tab-content').hide();
+        $('.'+target).show();
     });
+
+    // var tab = $('.tab');
+    // tab.click(function(){
+    //     var target = $(this).attr('href');
+    //     console.log("эта ссылка ведет на " + target);
+    //     $('.content').hide();
+    //     $(target).show();
+    // });
 });
