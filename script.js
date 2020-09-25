091079eb65541f8cebbcb99ccd44b7a77f9868f8
StGreg13 <13mrgreg@gmail.com>
2020-09-25 13:10:12 +0300

    end of tabs

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 $(document).ready(function(){
-    var tab = $('.tab');
-    tab.click(function(){
-        var target = $(this).attr('data-target');
-        $('.tab-content').hide();
-        $('.'+target).show();
-    });
+    // var tab = $('.tab');
+    // tab.click(function(){
+    //     var target = $(this).attr('data-target');
+    //     $('.tab-content').hide();
+    //     $('.'+target).show();
+    // });
 
     // var tab = $('.tab');
     // tab.click(function(){
@@ -13,4 +13,15 @@ $(document).ready(function(){
     //     $('.content').hide();
     //     $(target).show();
     // });
+
+    // homework
+
+    var tab = $('.tab');
+    tab.click(function(){
+        var target = $(this).attr('data-target');
+        $(".tab").removeClass('tab-active');
+        $(this).addClass('tab-active');
+        $('.tab-content').hide();
+        $('.'+target).show();
+    });
 });
